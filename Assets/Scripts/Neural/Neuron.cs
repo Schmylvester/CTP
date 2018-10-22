cf7d966ff5e9ffd123db1d4248c3e407958c9edb
TwelveSharks <thomas2.sylvester@live.uwe.ac.uk>
2018-10-22 17:04:24 +0100

    Started a new net

---
+++ Assets/Scripts/Neural/Neuron.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Type
+{
+    INPUT,
+    HIDDEN,
+    OUTPUT
+}
+
+public class Neuron : MonoBehaviour
+{
+    [SerializeField] Type type;
+    [SerializeField] Synapse[] synapses;
+    [SerializeField] float strength;
+    bool signal_sent = false;
+
+    private void Start()
+    {
+        strength = Random.Range(0.0f, 1.0f);
+        GetComponentInChildren<UnityEngine.UI.Text>().text = strength.ToString("C2");
+        if(type == Type.INPUT)
+        {
+            sendSignal();
+        }
+    }
+
+    void sendSignal()
+    {
+        if (!signal_sent)
+        {
+            signal_sent = true;
+            foreach (Synapse s in synapses)
+            {
+                s.sendSignal(strength);
+            }
+        }
+    }
+
+    public void receiveSignal(float signal)
+    {
+        strength += signal;
+        GetComponentInChildren<UnityEngine.UI.Text>().text = strength.ToString("C2");
+        if (type == Type.HIDDEN && strength > 1)
+        {
+            sendSignal();
+        }
+    }
+}
